bd3a42e260c057fe55394898c2c4fb35d5e80ec1
Jakub Strakos <94374098+Efestrix@users.noreply.github.com>
2023-09-24 21:07:46 +0200

    Update BasicCalculator.cs

--- BasicCalculator.cs
+++ BasicCalculator.cs
@@ -12,51 +12,87 @@ namespace CalculatorLib;
 //ReadFromMemory - nacteni z pameti, vraci obsah pameti
 //AddToMemory - pricte hodnotu k hodnote v pameti a opet ulozi do pameti, 1 parametr
 //DeleteMemory - vymaze pamet, tj. nastavi na 0
-public class BasicCalculator
+public class BasicCalculator : ICalculator
 {
-    public BasicCalculator()
+    private int _memory = 0;
+
+    public int Memory
     {
+        get {return _memory;}
+        set {_memory = value;}
     }
+
+    //Addition - 2 parametry a provede soucet, vrati vysledek
+    public int Addition(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+    //Substraction - 2 parametry, odcitani, vrati vysledek
+    public int Substriction(int number1, int number2)
+    {
+        return number1 - number2;
+    }
+    
+    //Multiplication - 2 parametry, nasobeni, vrati vysledek
+    public int Multiplication(int number1, int number2)
+    {
+        return number1 * number2;
+    }
+
+    //Division - 2 parametry, deleni, vrati vysledek
+    public int Division(int number1, int number2)
+    {
+        if  (number2 == 0)
+            //TODO: zde by mela byt vyhozena vyjimka, ale zatim neumime
+            return 0;
+        else
+            return number1 / number2;
+    }
+
+    //Negation - 1 parametr, vrati hodnotu s opacnym znamenkem
+    public int Negation(int number1)
+    {
+        return -number1;
+    }
+
+    //SaveToMemory - ulozeni do pameti, 1 parametr
+    public int SaveToMemory(int number1)
+    {
+        Memory = number1;
+        return Memory;
+    }
+
+    //ReadFromMemory - nacteni z pameti, vraci obsah pameti
+    public int ReadFromMemory()
+    {
+        return Memory;
+    }
+
+    //AddToMemory - pricte hodnotu k hodnote v pameti a opet ulozi do pameti, 1 parametr
+    public int AddToMemory(int number)
+    {
+       /* 
+       Memory = Memory + number;
+        return Memory;
+        */
+
+        return SaveToMemory(Addition(Memory,number));
+    }
+
+    
+    
+
+    //DeleteMemory - vymaze pamet, tj. nastavi na 0
+    public int DeleteMemory()
+    {
+        /*
+        Memory = 0;
+        return Memory;
+        */
+
+        return SaveToMemory(0);
+    }
+
 }
-
-Console.WriteLine("Zadej operaci");
-  char operace = Console.ReadLine()[0];
-
-  Console.WriteLine("Zadej dvě čísla");
-  int num1 = Convert.ToDouble(Console.ReadLine());
-  int num2 = Convert.ToDouble(Console.ReadLine());
-
-  int result =
-
-  switch(operace)
-  {
-     // součet 
-   case '+':
-       Console.WriteLine("{0} + {1} = {2}",num1,num2,(num1+num2));
-       break;
-
-     // odčitání 
-    case '-':
-       Console.WriteLine("{0} - {1} = {2}",num1,num2,(num1-num2));
-       break;
-  
-     // násobení
-    case '*':
-       Console.WriteLine("{0} * {1} = {2}",num1,num2,(num1*num2));
-       break;
-
-     // dělení
-    case '/':
-        if (num2 == 0.0)
-            Console.WriteLine("Dělení s nulou");
-        else 
-            Console.WriteLine("{0} / {1} = {2}",num1,num2,(num1/num2));
-        break;
-
-    default:
-         Console.WriteLine("{0} je neplatný operátor",op);
-         break;
-
-  {
-
   
