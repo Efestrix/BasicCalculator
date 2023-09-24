c6f1c39bab7e1ca94d769af90e0185fb4b788a23
Jakub Strakos <94374098+Efestrix@users.noreply.github.com>
2023-09-24 21:08:55 +0200

    Create ICalculator.cs

---
+++ ICalculator.cs
@@ -0,0 +1,10 @@
+namespace CalculatorLib;
+
+public interface ICalculator
+{
+    int Addition(int number1, int number2);
+    int Substriction(int number1, int number2);
+    int Multiplication(int number1, int number2);
+    int Division(int number1, int number2);
+    int Negation(int number1);
+}
